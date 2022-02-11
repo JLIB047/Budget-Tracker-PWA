63981920b2c553f681253e450208fb0a6015cf56
Jason Libertelli <jason.libertelli@gmail.com>
2022-02-11 12:18:39 -0500

    update heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+    '/',
     './index.html',
     './css/styles.css',
     './js/index.js',
@@ -25,39 +26,57 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (keyList){
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            cacheKeeplist.push(CACHE_NAME);
-
+        caches.keys().then(keyList => {
             return Promise.all(
-                keyList.map(function(key, i){
-                    if(cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== APP_PREFIX) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
                     }
                 })
             );
         })
     );
+        
+          self.clients.claim();
 });
 
 self.addEventListener('fetch', function (e) {
+    if(e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches 
+                .open(CACHE_NAME)
+                .then(cache => {
+                    return fetch(e.request)
+                        .then(response => {
+                            if(response.status === 200) {
+                                cache.put(e.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(e.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
+        fetch(e.request).catch(function() {
+            return caches.match(e.request).then(function(response) {
+                if(response) {
+                    return response;
+                } else if (e.request.headers.get('accept').includes('text/html')){
+                    return caches.match('/');
+                }
+            });
         })
-    )
+    );
 });
